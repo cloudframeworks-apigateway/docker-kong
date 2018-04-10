a059deb66f14200499f91b34cb8ca4942f15b367
barnett <zengqg@goodrain.com>
2018-04-10 18:55:55 +0800

    init

---
+++ docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+# Disabling nginx daemon mode
+export KONG_NGINX_DAEMON="off"
+
+# Setting default prefix (override any existing variable)
+export KONG_PREFIX="/usr/local/kong"
+
+# Prepare Kong prefix
+if [ "$1" = "/usr/local/openresty/nginx/sbin/nginx" ]; then
+	kong prepare -p "/usr/local/kong"
+fi
+# Prepare database
+for (( i = 0; i < 20; i++))
+do 
+   kong migrations up
+   if [ $? -eq 0 ];then
+      break
+   fi
+   sleep 2
+done
+
+exec "$@"
