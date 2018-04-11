60cc421d7a511b6a5a9c1222921b71892a8a5482
barnett <zengqg@goodrain.com>
2018-04-11 10:14:40 +0800

    loop waiting postgres db ready

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # Disabling nginx daemon mode
 export KONG_NGINX_DAEMON="off"
@@ -9,15 +8,19 @@ export KONG_PREFIX="/usr/local/kong"
 
 # Prepare Kong prefix
 if [ "$1" = "/usr/local/openresty/nginx/sbin/nginx" ]; then
-	kong prepare -p "/usr/local/kong"
+	kong prepare -p "/usr/local/kong" || { echo "kong prepare failed"; exit 1; }
 fi
 # Prepare database
 for (( i = 0; i < 20; i++))
-do 
-   echo "step $i exec kong migrations up"
+do
    kong migrations up
    if [ $? -eq 0 ];then
       break
+   fi
+   echo "step $i exec kong migrations up failure, retry..."
+   if [ $i -eq 19 ];then
+      echo "waiting postgres db ready timeout. exist"
+      exit 1
    fi
    sleep 2
 done
